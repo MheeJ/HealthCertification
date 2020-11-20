e66e0e746a35b7da7a3f2fb7ee9476e742387cbc
HaengG <khj951007@naver.com>
2020-11-20 15:47:18 +0900

    edit date

--- app/src/main/java/com/example/healthcertification/ui/MyHealth_Fragment/MyHealth_HealthCalculation.java
+++ app/src/main/java/com/example/healthcertification/ui/MyHealth_Fragment/MyHealth_HealthCalculation.java
@@ -140,7 +140,7 @@ public class MyHealth_HealthCalculation extends Fragment implements View.OnClick
         String goodweight2_Str = String.format("%.2f",goodweight2);
         long now = System.currentTimeMillis();
         Date mDate = new Date(now);
-        SimpleDateFormat simpleDate = new SimpleDateFormat("yy-MM-dd");
+        SimpleDateFormat simpleDate = new SimpleDateFormat("yy.MM.dd");
         getTime = simpleDate.format(mDate);
 
         //HC_ListView_Adapter.addItem(getTime,height, weight, BmiStr, my_state);
